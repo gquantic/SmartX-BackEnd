95cf5c0db65a0dd9c6648a2a0d2bb428757d8184
Murad Gapurovich <darkkukri0550@gmail.com>
2022-07-24 16:00:38 +0300

    User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @method static find()
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -43,4 +46,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function referral()
+    {
+        $this->hasOne(User::class);
+    }
+
+    public function parts()
+    {
+        $this->hasMany(Part::class);
+    }
 }
