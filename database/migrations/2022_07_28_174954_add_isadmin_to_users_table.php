b3711d08893e56f8898d7508be53da8451e54569
Murad Gapurovich <darkkukri0550@gmail.com>
2022-08-08 12:38:54 +0300

    Правка

--- database/migrations/2022_07_28_174954_add_isadmin_to_users_table.php
+++ database/migrations/2022_07_28_174954_add_isadmin_to_users_table.php
@@ -14,7 +14,7 @@ class AddIsadminToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_admin');
+            $table->boolean('is_admin')->default(false)->nullable();
         });
     }
 
